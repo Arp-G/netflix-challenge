091f4c6429ab6d3d268b42e872bbff0821c4f274
Arpan <arpanghoshal3@gmail.com>
2020-10-10 22:35:13 +0530

    finish parallel file loadinf implementation

--- src/data_loaders.rs
+++ src/data_loaders.rs
@@ -1,24 +1,48 @@
 use std::collections::HashMap;
 use std::fs::File;
 use std::io::{prelude::*, BufReader};
+use std::sync::mpsc;
+use std::thread;
 // use crate::helpers;
 
 #[derive(Debug)]
 pub struct Rating {
     rating: u8,
-    centered_rating: u8
+    centered_rating: u8,
 }
 
 impl Rating {
     pub fn new(rating: u8) -> Rating {
         Rating {
             rating: rating,
-            centered_rating: 0
+            centered_rating: 0,
         }
     }
 }
 
-pub fn load(file_path: &str) -> HashMap<u32, HashMap<u32, Rating>> {
+pub fn parallel_loader(files: Vec<&'static str>) -> HashMap<u32, HashMap<u32, Rating>> {
+    let (tx, rx) = mpsc::channel();
+
+    for file in &files {
+        let sender = mpsc::Sender::clone(&tx);
+
+        let file = file.clone();
+        thread::spawn(move || {
+            sender.send(load(file)).unwrap();
+        });
+    }
+
+    let mut files_iter = rx.iter();
+    let mut combined_ratings = files_iter.next().unwrap();
+
+    combine_hashes(&mut combined_ratings, files_iter.next().unwrap());
+    combine_hashes(&mut combined_ratings, files_iter.next().unwrap());
+    combine_hashes(&mut combined_ratings, files_iter.next().unwrap());
+
+    combined_ratings
+}
+
+fn load(file_path: &str) -> HashMap<u32, HashMap<u32, Rating>> {
     let file = File::open(file_path).unwrap();
     let reader = BufReader::new(file);
 
@@ -32,7 +56,6 @@ pub fn load(file_path: &str) -> HashMap<u32, HashMap<u32, Rating>> {
             result.pop(); // remove ":"
             movie_id = result.parse::<u32>().unwrap();
         } else {
-
             let mut iter = result.split(",");
             let user_id = iter.next().unwrap().parse::<u32>().unwrap();
 
@@ -44,6 +67,23 @@ pub fn load(file_path: &str) -> HashMap<u32, HashMap<u32, Rating>> {
         }
     }
 
-    println!("{:?}", ratings_hash);
+    println!("Finished processing file {}", file_path);
     ratings_hash
+}
+
+fn combine_hashes(
+    combined_ratings: &mut HashMap<u32, HashMap<u32, Rating>>,
+    new_ratings: HashMap<u32, HashMap<u32, Rating>>,
+) {
+    for (user_id, movie_ratings_for_user) in new_ratings {
+        match combined_ratings.get_mut(&user_id) {
+            Some(old_hash_map) => {
+                old_hash_map.extend(movie_ratings_for_user.into_iter());
+            }
+
+            None => {
+                combined_ratings.insert(user_id, movie_ratings_for_user);
+            }
+        };
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,27 @@
-
-
 mod data_loaders;
 mod helpers;
+use std::time::SystemTime;
 
 fn main() {
-    data_loaders::load("data/combined_data_1.txt");
+    let now = SystemTime::now();
+
+    // Serailly loading files takes 53 seconds
+    // Concurrently loading files takes 17 seconds
+    // Merging hashes takes ~4 seconds, 
+    // so total time to load data parallelly = 21 seconds(release mode) (221 seconds in debug mode)
+    let _data = data_loaders::parallel_loader(vec![
+        "data/combined_data_1.txt",
+        "data/combined_data_2.txt",
+        "data/combined_data_3.txt",
+        "data/combined_data_4.txt",
+    ]);
+
+    match now.elapsed() {
+        Ok(elapsed) => {
+            println!("{}", elapsed.as_secs());
+        }
+        Err(e) => {
+            println!("Error: {:?}", e);
+        }
+    }
 }
